93f596430899a52707d71f03ffd3c1ec5667806a
amitdevopsschool <amitcotocus72@gmail.com>
2023-10-26 16:29:45 +0530

    add

---
+++ src/effects/strokesOpacityAll.js
@@ -0,0 +1,13 @@
+/**
+ * Paint the canvas with strokes in all directions 
+ * @param {Object} main
+ */
+var parameters = [];
+var exec = function(main, params, callback) {
+  main.applyEffect('strokesOpacityLeft');
+  main.applyEffect('strokesOpacityRight');
+  main.applyEffect('strokesOpacityVertical');
+  main.applyEffect('strokesOpacityHorizontal');
+
+  callback.call();
+};
